b79f27efd70d6ab4ec415288b49fd4aa49aae3a6
hieupqgcd19971 <hieupqgcd19971@fpt.edu.vn>
2022-05-09 20:20:17 +0700

    email sender

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using AppDev1.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using AppDev1.Email;
+
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("UserContextConnection");;
 
@@ -13,6 +16,17 @@ builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireCo
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var config = builder.Configuration;
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+builder.Services.Configure<EmailSenderOptions>(options =>
+{
+    options.Host = config["MailSettings:Host"];
+    options.Port = int.Parse(config["MailSettings:Port"]);
+    options.User = config["MailSettings:User"];
+    options.Pass = config["MailSettings:Pass"];
+    options.Name = config["MailSettings:Name"];
+    options.Sender = config["MailSettings:User"];
+});
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
